8321d804f25e2acdd64373263eeb42424d89a0df
rajbhoyar729 <rabhoyarr729@gmail.com>
2024-06-30 00:30:13 +0530

    init - 2

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+  <main className='relative left-0 bg-black-100 flex justify-center items-center flex-col overflow-hidden  max-w-full'>
+<div className="max-w-7xl">
+  <Hero />
+</div>
+  </main>
+  );
+}
