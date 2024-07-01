28f0ca08f575d15a5c396124ded2716acf2fcb62
rajbhoyar729 <rabhoyarr729@gmail.com>
2024-07-01 17:37:28 +0530

    init - 2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const hero = () => {
         </p>
         <div className='max-w-5xl max-h-24 flex flex-col justify-center items-center relative'>
         <TextGenerateEffect 
-        className=''
+        className='opacity-60 text-transparent'
         words="2024 graduate,seeking for opportunities , believe in tackling challenges head-on which fuel's intellectual growth and sharpen problem-solving abilities.  open to collaborate on challenging projects to  create solutions."
         />
         </div>
