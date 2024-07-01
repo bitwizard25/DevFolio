3eb8eb79c8348b23711e3c6380b3f40795e9a1da
rajbhoyar729 <rabhoyarr729@gmail.com>
2024-07-01 16:01:49 +0530

    init - 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import Hero from "@/components/Hero";
+import Main from "@/components/Main";
+import LocomotiveScroll from 'locomotive-scroll';
+
 
 export default function Home() {
+  
   return (
-  <main className='relative left-0 bg-black-100 flex justify-center items-center flex-col overflow-hidden  max-w-full'>
-<div className="max-w-7xl">
+  <main  className='max-w-full max-h-full relative left-0 bg-black-100 flex justify-center items-center flex-col overflow-hidden '>
+<div className="max-w-fll">
   <Hero />
+  <Main />
 </div>
   </main>
   );
+
+  
 }
