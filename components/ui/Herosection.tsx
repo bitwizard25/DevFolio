e9926b355e7b5cd7f3509e9377c2b93f88b0fc13
rajbhoyar729 <rbhoyar729@gmail.com>
2024-09-11 22:15:10 +0530

    rc

--- components/ui/Herosection.tsx
+++ components/ui/Herosection.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import {   Code, Database, Server } from 'lucide-react';
+import { Code, Database, Server } from 'lucide-react';
 
 const HeroSection = () => {
   const [terminalText, setTerminalText] = useState('');
