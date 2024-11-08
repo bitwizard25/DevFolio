3cac56bba93ccfcc2e2319c2762cbb1a642a4a1d
Raj bhoyar <85496077+rajbhoyar729@users.noreply.github.com>
2024-11-08 14:44:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-
-import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/ui/Herosection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <main className='remove-scrollbar'>
-  <HeroSection/>
-  </main>
+    <main className="remove-scrollbar">
+      <HeroSection />
+    </main>
   );
 }
